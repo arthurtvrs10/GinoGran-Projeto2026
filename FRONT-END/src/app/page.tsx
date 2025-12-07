6b4851d5e9843a1243643c889bdfebf50e8834f2
arthurtvrs10 <arthurtavares0222@gmail.com>
2025-12-07 19:16:18 -0300

    Initial commit

---
+++ FRONT-END/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "../Components/Header";
+
+export default function Home() {
+  return (
+    <><h1 >helllo</h1>
+      <div className=" bg w-3.5 h-2.5">
+      </div>
+    
+    </>
+  );
+}
